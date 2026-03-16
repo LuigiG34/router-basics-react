72545dbb447235ab0be9ca35fae6e585a54c56f9
Luigi Gandemer <luigigandemer6@gmail.com>
2026-03-16 17:10:01 +0100

    Navigating between pages and links

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-const routeDef = createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/products" element={<ProductsPage />} />
-    </Route>
-);
-const router = createBrowserRouter(routeDef);
+// const routeDef = createRoutesFromElements(
+//     <Route>
+//       <Route path="/" element={<HomePage />} />
+//       <Route path="/products" element={<ProductsPage />} />
+//     </Route>
+// );
+// const router = createBrowserRouter(routeDef);
 
-// const router = createBrowserRouter([
-//   {path: '/', element: <HomePage />},
-//   {path: '/products', element: <ProductsPage />},
-// ])
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage />},
+  {path: '/products', element: <ProductsPage />},
+])
 
 
 function App() {
