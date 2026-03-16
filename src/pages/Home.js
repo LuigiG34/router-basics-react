8935b85b70dddbbc1f825f0953a33d9ecec2081d
Luigi Gandemer <luigigandemer6@gmail.com>
2026-03-16 17:04:20 +0100

    Exploring how to make routes

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+function HomePage(){
+    return (
+        <h1>My homepage !</h1>
+    )
+}
+
+export default HomePage;
---
+++ src/pages/Products.js
@@ -0,0 +1,7 @@
+function ProductsPage(){
+    return (
+        <h1>The products page !</h1>
+    )
+}
+
+export default ProductsPage;
